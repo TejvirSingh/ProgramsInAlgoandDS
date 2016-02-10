8dec5f6399de9ae266be1641b2d495b4ff52d50b
Tejvir Singh <tejvirsaggu@yahoo.co.in>
2016-02-09 18:35:34 -0600

    Changes in union_find.. and a new program..

--- union_find_test.c
+++ union_find_test.c
@@ -21,34 +21,84 @@
 /* returns the set id of the object. */
 int find(int object, int id[])
 {
+  
   int next_object;
-  next_object = id[object];
+  next_object = object;
 
-  //printf("\nFind root of %d\n\t", object);
-  //printf("   %d ", next_object);
   while (next_object != id[next_object])
   {
     next_object = id[next_object];
 	//printf("   %d ", next_object);
   }
-
+  /*
+  **************************************
+     Changes made in the CODE
+  */
+  while(object != next_object)
+  {
+     int new_object = id[object];
+     id[object] = next_object;
+     object = new_object;
+  }
+  /*
+  ***************************************
+  */
+  
   return next_object;
 }
 
+
+
 /* Unites the two sets specified by set_id1 and set_id2.
+   ************************************************************
+   
+   Changes in this function... 
+   
+   @param a  contains the p of the input pair
+   @param b  contains the q of the input pair
+   
  */
-void set_union(int set_id1, int set_id2, int id[], int size, int size_arr[])
+void set_union(int set_id1, int set_id2, int id[], int size, int size_arr[],int a, int b)
 {
-  if (size_arr[set_id1] < size_arr[set_id2])
+  int root_node_p = find(set_id1,id); //  Asking to find the root of the node p
+  int root_node_q = find(set_id2,id);  //  Asking to find the root of the node q
+  if(root_node_q == root_node_p) return;
+
+  if (size_arr[root_node_p] < size_arr[root_node_q])
   {
-	id[set_id1] = set_id2;
-	size_arr[set_id2] = size_arr[set_id1]  + size_arr[set_id2];
+	id[root_node_p] = root_node_q;
+	/*
+	*******************************
+	changes in this method.. 
+	*/
+	id[a] = root_node_q;
+	id[b] = root_node_q;
+	
+	printf(" if function ... Root node of p and q",id[root_node_p],id[root_node_q]);
+	/*
+	*******************************
+	*/
+	
+	size_arr[root_node_q] += size_arr[root_node_p];
   }
-  else
-  {
-	id[set_id2] = set_id1;
-	size_arr[set_id1] = size_arr[set_id1]  + size_arr[set_id2];
-  }
+  else 
+  	{
+	   id[root_node_q] = root_node_p;
+	   /*
+	*******************************
+	  Changes in this method...
+	*/
+	   id[b] = root_node_p;
+	   id[a] = root_node_p;
+	   printf(" else function ....  Root node of p %d and q %d and set_id1 %d and set_id2 %d ",id[root_node_p],id[root_node_q],a,b);
+	   
+	   /*
+	*******************************
+	*/
+	   size_arr[root_node_p] += size_arr[root_node_q];
+  	}
+  
+
 }
 
 
@@ -69,6 +119,7 @@ int main()
   {
     p_id = find(p, id);
     q_id = find(q, id);
+    
 
     if (p_id == q_id)
     {
@@ -77,8 +128,9 @@ int main()
       continue;
     }
 
-    set_union(p_id, q_id, id, N, size_arr);
+    set_union(p_id, q_id, id, N, size_arr,p,q);   // sending two extra values.. 
     printf(" %d %d link led to set union\n", p, q);
+    
     print_array_int(id, N);
     print_array_int(size_arr, N);
   }
